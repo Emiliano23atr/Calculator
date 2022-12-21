fe4613e43ae101a2d1e53aed247b2680e952d569
Emiliano23atr <joaovitoremiliano5@gmail.com>
2022-12-21 20:22:02 -0300

    Modo de saida da aplicação criado

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ class Program
         Console.WriteLine("2 - Subtração");
         Console.WriteLine("3 - Divisão");
         Console.WriteLine("4 - Multiplicação");
+        Console.WriteLine("5 - Sair");
 
         Console.WriteLine("----------");
         Console.WriteLine("Selecione uma opção: ");
@@ -32,6 +33,8 @@ class Program
             case 3: Divisao();
                 break;
             case 4: Multiplicacao();
+                break;
+            case 5: System.Environment.Exit(0);
                 break;
             default: Menu(); 
                 break;       
